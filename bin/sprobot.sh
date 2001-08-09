87b2a62fc01bc8b78bc52f417ebde12b59f1e73c
schlegel <schlegel@AlbireoPro.local>
2001-08-09 18:36:54 +0000

    No longer need explicit path for rsync on remote machine, since sos.apo has a good version installed.

--- bin/sprobot.sh
+++ bin/sprobot.sh
@@ -89,14 +89,14 @@ for mjdstr in $remotedir ; do
       # Copy the astrolog files...
       echo SPROBOT: rsync "$hostname:/astrolog/$mjdstr" $astrologdir
       rsync -ar --rsh="ssh -c arcfour" \
-       --rsync-path=/p/rsync/v2_4_3/rsync \
        "$hostname:/astrolog/$mjdstr" $astrologdir
+#       --rsync-path=/p/rsync/v2_4_3/rsync
 
       # Copy the raw FITS files... copy only files ending in ".fit.gz"
       echo SPROBOT: rsync "$hostname:/data/spectro/$mjdstr/*" $localdir
       rsync -ar --rsh="ssh -c arcfour" \
-       --rsync-path=/p/rsync/v2_4_3/rsync \
        "$hostname:/data/spectro/$mjdstr/*.fit.gz" $localdir
+#       --rsync-path=/p/rsync/v2_4_3/rsync
 #       "$hostname:/data/spectro/$mjdstr/*" $localdir
 
       # Compress the raw FITS files w/gzip...
