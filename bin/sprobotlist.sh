85d8590ed023944c3910e1d322e51894750f2ef0
schlegel <schlegel@AlbireoPro.local>
2002-03-01 16:33:12 +0000

    New script "sprobotlist.sh" which is a trimmed version of "sprobotcopy.sh", creating the plate list and summary files once per day.

---
+++ bin/sprobotlist.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#------------------------------------------------------------------------------
+# Script to create plate list and summary files once per day.
+#
+# D. Schlegel, Princeton, 1 Mar 2002
+#------------------------------------------------------------------------------
+# Generate summary files.
+
+echo ""
+echo "-------------------------------------------------------------------------------"
+echo "SPROBOTLIST: Started at "`date`
+
+cd $SPECTRO_DATA
+echo "platelist, /create" | idl
+echo "platemerge" | idl 2> /dev/null
+echo "platemerge, /qsurvey" | idl 2> /dev/null
+echo "platemerge, /public" | idl 2> /dev/null
+
+#------------------------------------------------------------------------------
