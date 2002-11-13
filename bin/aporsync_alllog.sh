c5710af27d380f10a2a050a7b3ddefd8c2c49d62
schlegel <schlegel@AlbireoPro.local>
2002-11-13 20:12:35 +0000

    New shell script aporsync_alllog.sh for copying all of the log files from sdsshost.apo:/astrolog/$MJD to the sos.apo machine. This will be done once per day at 7:45.  This rsync must follow symbolic links on sdsshost.apo, since it now looks like quite a few of the files there are now symbolic links. (That's crazy, but what's a guy to do?)

---
+++ bin/aporsync_alllog.sh
@@ -0,0 +1,57 @@
+#! /bin/sh
+
+#------------------------------------------------------------------------------
+# This routine is called by the cron daemon, as set up with "sos_start".
+# It syncs files from sdsshost to the local machine (i.e., sos.apo.nmsu.edu).
+#
+# Only sync the /astrolog/$MJD directories when there is a corresponding
+# /data/spectro/$MJD directory.  This is because the data directories are
+# periodically purged on sdsshost, whereas the astrolog directories are not.
+# For reducing data with SOS, we obviously only need the astrolog directories
+# that correspond to data that presently exists on disk.
+#
+# Note that the directory locations on sdsshost.apo are hardwired
+# as /astrolog and /data/spectro.
+#
+# We need the executable code "rsync" in the default path
+# (e.g., as /usr/bin/rsync).
+#
+# S. Burles, APO, 4 May 2000
+#------------------------------------------------------------------------------
+
+echo "APORSYNC_ALLLOG: Launched at "`date -u` UID=$UID PPID=$PPID
+
+if [ -z "$RAWDATA_DIR" ] ; then
+   echo "Abort: RAWDATA_DIR not set!"
+   exit
+fi
+
+if [ -z "$ASTROLOG_DIR" ] ; then
+   echo "Abort: ASTROLOG_DIR not set!"
+   exit
+fi
+
+# This syncs /astrolog/[5-9]???? from sdsshost to the local machine.
+# Only consider those MJD subdirectories /astrolog/$MJD where a corresponding
+# data directory exists in /data/spectro/$MJD.
+# Copy all log files in that directory, including files not
+# copied by "aporsync_logs.sh".
+# We follow symbolic links in this copy (--copy-unsafe-links),
+# since it now appears that some brain surgeon decided that some
+# subset of these files should live elsewhere, with only symbolic
+# links in the /astrolog/$MJD directory.
+
+datadirs=`ssh sdsshost.apo.nmsu.edu ls -d /data/spectro/[5-9]????`
+astrologdirs=`echo $datadirs | sed -n 's/\/data\/spectro/\/astrolog/pg'`
+
+for thisdir in $astrologdirs
+do
+   echo Copy all log files from $thisdir
+   rsync -ar --rsh="ssh -c blowfish" \
+    --rsync-path=/p/rsync/v2_4_3/rsync \
+    --copy-unsafe-links \
+    sdsshost.apo.nmsu.edu:$thisdir $ASTROLOG_DIR
+done
+
+echo "APORSYNC_ALLLOG: Finished at "`date -u` UID=$UID PPID=$PPID
+
