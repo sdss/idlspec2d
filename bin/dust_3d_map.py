1e6caae586c52b330aa5d04cafeedb3ec6852689
hjibarram <hjibarram@gmail.com>
2021-08-08 23:17:33 -0500

    update dust3d_maps
    Use the local query of the 3d dustmaps instead of the web query

--- bin/dust_3d_map.py
+++ bin/dust_3d_map.py
@@ -4,6 +4,7 @@ warnings.filterwarnings("ignore")
 from astropy.coordinates import SkyCoord
 import astropy.units as units
 from dustmaps.bayestar import BayestarWebQuery
+from dustmaps.bayestar import BayestarQuery
 import numpy as np
 import sys
 
@@ -12,7 +13,8 @@ sys.argv=list(filter(None,sys.argv))
 ll=np.float(sys.argv[1])
 bb=np.float(sys.argv[2])
 rr=np.float(sys.argv[3])
-bayestar = BayestarWebQuery(version='bayestar2015')
+#bayestar = BayestarWebQuery(version='bayestar2015')
+bayestar = BayestarQuery(version='bayestar2015')
 coords = SkyCoord(ll*units.deg, bb*units.deg,distance=rr*units.pc, frame='galactic')
 reddening = bayestar(coords, mode='median')#'percentile',pct=70.)#mode='median')
 #reddening1 = bayestar(coords, mode='percentile',pct=70.)
