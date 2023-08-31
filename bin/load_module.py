b72a5ed3518fd8bf5116916e4d548d99f3163c01
Sean-Morrison <smorris0@illinois.edu>
2023-08-31 18:06:27 -0500

    Update load_module.py

--- bin/load_module.py
+++ bin/load_module.py
@@ -1,6 +1,9 @@
 import os.path as ptt
+from os import getenv
 import importlib
 import sys
+
+
 def import_module(path = '/uufs/chpc.utah.edu/sys/pkg/modules/init/python3'):
     spec = importlib.util.spec_from_loader(ptt.basename(path),
                 importlib.machinery.SourceFileLoader(ptt.basename(path), path))
@@ -8,6 +11,15 @@ def import_module(path = '/uufs/chpc.utah.edu/sys/pkg/modules/init/python3'):
     spec.loader.exec_module(pymodule)
     sys.modules[ptt.basename(path)] = pymodule
     return(pymodule)
+
+def load_env(key, default=None):
+    val = getenv(key)
+    if val is None:
+        val = default
+        if val is None:
+            print('ERROR: '+key+' is not set')
+            exit()
+    return(val)
 
 
 def load_module():
