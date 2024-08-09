997829f4ade5f6adb78bccca64accefdf8d07a62
Sean-Morrison <smorris0@illinois.edu>
2024-08-09 01:08:17 -0500

    Create cronrun.bash
    Added a wrapper to load modules and run the pipeline within crontabs at Utah

---
+++ bin/cronrun.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# cronrun
+#
+# Designed to load the correct module and execute the daily commands
+#
+# usage: cronrun module "script"
+#
+# Created by Sean Morrison on 2/20/24.
+
+function usage {
+    local execName=$(basename $0)
+    (
+    echo "usage: $execName module 'script'"
+    echo " "
+    ) >&2
+    exit 1
+}
+
+# Check for the -h flag
+while getopts "h" flag; do
+    case "$flag" in
+        h) usage ;;
+    esac
+done
+
+# Extract the module and script from the arguments
+ARG1=${@:$OPTIND:1}
+ARG2=${@:$OPTIND+1:1}
+
+# Ensure both arguments are provided
+if [ -z "$ARG1" ] || [ -z "$ARG2" ]; then
+    usage
+fi
+
+# Load the specified module and execute the script
+module purge
+export MODULE="$ARG1"
+module load "$ARG1"
+eval "$ARG2"
