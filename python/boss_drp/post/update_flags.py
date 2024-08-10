28ba6877ec2dfd7dd19928cdb823d48bfe42aa53
Sean-Morrison <smorris0@illinois.edu>
2024-08-10 00:51:43 -0500

    Update update_flags.py

--- python/boss_drp/post/update_flags.py
+++ python/boss_drp/post/update_flags.py
@@ -8,15 +8,17 @@ try:
     from sdssdb.peewee.sdss5db.targetdb import database
     test = database.set_profile(load_env('DATABASE_PROFILE', default='pipelines'))
     from sdssdb.peewee.sdss5db.targetdb import CartonToTarget, Carton, Version, Mapper, Target
+    nodb=False
 except:
     if load_env('DATABASE_PROFILE', default='pipelines').lower() in ['pipelines','operations']:
         splog.log('ERROR: No SDSSDB access')
         exit()
     else:
-        splog.log('WARNING: No SDSSDB access')
+        splog.log('ERROR: No SDSSDB access')
+    nodb= True
 
 import argparse
-from astropy.table import Table, Column
+from astropy.table import Table, Column, join
 from astropy.io import fits
 from os import getenv, remove
 import os.path as ptt
@@ -33,10 +35,14 @@ def get_Targeting_file(run2d, boss_spectro_redux=getenv('BOSS_SPECTRO_REDUX')):
 
 
 def update_Targeting_flags(run2d, boss_spectro_redux, schema = None, clobber=False,
-                           nobackup=False):
+                           nobackup=False,nrows=None, spall_tab=None, idcol='CATALOGID',
+                           legacy=False):
+    if nodb:
+        exit()
     spall_file = ptt.join(boss_spectro_redux, run2d, 'summary','daily', f'spAll-lite-{run2d}.fits.gz')
     Targeting_file = get_Targeting_file(run2d, boss_spectro_redux=boss_spectro_redux)
-    
+    if not ptt.exists(Targeting_file):
+        clobber = True
     if clobber:
         splog.info('Determining Updated Flags')
         spall = fits.getdata(spall_file, 1)
@@ -61,22 +67,17 @@ def update_Targeting_flags(run2d, boss_spectro_redux, schema = None, clobber=Fal
         else:
             raise Exception(Targeting_file+' is missing')
 
-    run2d_dir = ptt.join(boss_spectro_redux, run2d)
+    run2d_dir = ptt.join(boss_spectro_redux, run2d, 'summary')
     files = ['spAll-lite-{run2d}{schema}.fits.gz','spAll-{run2d}{schema}.fits.gz']
-#    if schema is not None:
-#        custom = schema
-#        schema = [None]
-#        schema.extend(custom)
-#    else:
-#        schema = [None]
         
     epochs = ['daily','epoch']
     if schema is not None:
         epochs.extend(schema)
     for epoch in epochs:
         if schema is not None:
-            sc = '' if epoch != schema else '-'+sc
-
+            sc = '' if epoch in ['daily'] else '-'+epoch
+        else:
+            sc = '' if epoch in ['daily'] else '-'+epoch
         for f in files:
             ff = ptt.join(run2d_dir,epoch,f.format(run2d=run2d, schema = sc))
             
@@ -101,23 +102,25 @@ def update_Targeting_flags(run2d, boss_spectro_redux, schema = None, clobber=Fal
                         splog.info(f'Padding SDSS5_TARGET_FLAGS in {ptt.basename(ff)}')
                         coldat = hdul[1].data['SDSS5_TARGET_FLAGS']
                         pad = update_shape - spall_shape
-#                            hdul[1].data['SDSS5_TARGET_FLAGS'] = np.pad(coldat,[(0,0),(pad,0)], mode = 'constant', constant_values= 0)
-#                            hdul[1].data['SDSS5_TARGET_FLAGS'] = fits.Column(name='SDSS5_TARGET_FLAGS', format=f'{update_shape}B',
-#                                                                            array=np.pad(coldat,[(0,0),(pad,0)], mode = 'constant', constant_values= 0))
                         new_col = fits.Column(name='SDSS5_TARGET_FLAGS', format=f'{update_shape}B',
-                                              array=np.pad(coldat,[(0,0),(pad,0)], mode = 'constant', constant_values= 0))
+                                              array=np.pad(coldat,[(0,0),(0,pad)], mode = 'constant', constant_values= 0))
                         new_col = fits.ColDefs([new_col])
                         old_cols = hdul[1].columns
                         colidx = np.where(np.asarray(old_cols.names) == 'SDSS5_TARGET_FLAGS')[0][0]
                         
                         hdul[1] = fits.BinTableHDU.from_columns(old_cols[0:colidx-1]+new_col+old_cols[colidx+1:],
                                                                 header=hdul[1].header)
-                    
-                    
-                    search_table = join(Table([hdul[1].data['SDSS_ID']], search_table), keys='SDSS_ID',join_type='left')
-                    hdul[1].data['SDSS5_TARGET_FLAGS'][nf] = search_table['SDSS5_TARGET_FLAGS'][nf]
-                    #hdul[1].data['SDSS5_TARGET_FLAGS'][nf] = search_table['SDSS5_TARGET_FLAGS'][idx]
-                    ##hdul[1].data['SDSSC2BV'] = search_table['SDSSC2BV'][idx][nf]
-                    hdul[0].header['SDSSC2BV'] = search_table['SDSSC2BV'][0]
+                    elif spall_shape > update_shape:
+                        pad = spall_shape - update_shape
+                        coldat = search_table['SDSS5_TARGET_FLAGS'].data
 
+                        search_table['SDSS5_TARGET_FLAGS'] = np.pad(coldat, [(0,0),(0,pad)],
+                                                                    mode = 'constant',
+                                                                    constant_values= 0)
+
+
+                    search_table_j = join(Table([hdul[1].data['SDSS_ID'], range(len(hdul[1].data))], names=['SDSS_ID','order']), search_table, keys='SDSS_ID',join_type='left')
+                    search_table_j.sort('order')
+                    hdul[1].data['SDSS5_TARGET_FLAGS'][nf] = search_table_j['SDSS5_TARGET_FLAGS'][nf]
+                    hdul[0].header['SDSSC2BV'] = search_table_j['SDSSC2BV'][0]
 
