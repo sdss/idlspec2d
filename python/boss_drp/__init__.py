100ec2416876fd60ef57e78f30088cc9e64192b0
Sean-Morrison <smorris0@illinois.edu>
2024-08-29 12:25:57 -0500

    Misc updates

--- python/boss_drp/__init__.py
+++ python/boss_drp/__init__.py
@@ -6,8 +6,11 @@ from sdsstools import get_package_version
 #from sdsstools import get_config, Configuration
 import os
 import numpy as np
-
+import warnings
 __version__ = get_package_version(__file__, 'boss_drp') or 'dev'
+
+
+
 
 try:
     daily_dir = os.getenv('DAILY_DIR')
@@ -17,4 +20,20 @@ except:
 if daily_dir is None:
     daily_dir = os.path.join(os.getenv('HOME'),'daily')
     os.environ['DAILY_DIR'] = daily_dir
-    
+
+class MissingEnvVarWarning(UserWarning):
+    pass
+
+try:
+    idlspec2d_dir = os.getenv('IDLSPEC2D_DIR')
+except:
+    idlspec2d_dir = None
+if idlspec2d_dir is None:
+    warnings.warn('IDLSPEC2D_DIR ENV Variable is not set',MissingEnvVarWarning)
+    os.environ['IDLSPEC2D_DIR']  = os.path(os.path.dirname(os.path.dirname(__file__)))
+
+idlspec2d_dir = os.getenv('IDLSPEC2D_DIR')
+
+
+
+favicon ="https://www.sdss.org/wp-content/uploads/2022/04/cropped-cropped-site_icon_SDSSV-192x192.png"
