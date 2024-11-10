1c42115c0680513feaaa415bb028b8b19afa5d01
Sean-Morrison <smorris0@illinois.edu>
2024-11-10 11:19:48 -0600

    Update fieldmerge.py

--- python/boss_drp/post/fieldmerge.py
+++ python/boss_drp/post/fieldmerge.py
@@ -1113,8 +1113,6 @@ def write_spAll(spAll, spline, spAll_lite, indir, run2d, datamodel, line_datamod
         hdul = None
         if spAll_lite is not None:
             spAll = Table(spAll.data)
-            spAll_lite_cols = spAll_lite.colnames
-                        spAll_lite_cols
             spAll.remove_columns(list(filter(lambda x: x not in ['delta_ra','delta_dec'], spAll_lite.colnames)))
             spall.remove_columns(['DELTA_RA_LIST','DELTA_DEC_LIST'])
             spAll = hstack([spAll, spAll_lite], join_type = 'exact')
