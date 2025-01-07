3ea502fd3bd5393edd09de81b595c089808bca94
Sean-Morrison <smorris0@illinois.edu>
2025-01-06 22:05:13 -0600

    Update grep.py
    Add option to return the line from grep

--- python/boss_drp/utils/grep.py
+++ python/boss_drp/utils/grep.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import mmap
 
-def grep(filepath, grepstr):
+
+def grep(filepath, grepstr, line=False):
     with open(filepath, 'rb', 0) as f:
         s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
         if s.find(grepstr.encode('UTF-8')) != -1:
-            return(True)
-    return(False)
+            if line:
+                s.seek(0)  # Reset position to start of the file
+                for ln in s.read().decode('UTF-8').splitlines():
+                    if grepstr in ln:
+                        return (True, ln)
+            return True  # Found, but line=False
+    return (False, None) if line else False
